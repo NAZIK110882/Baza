1a880efda6f2993743f41b84dfb08d462321325e
nazar <nazaron11jac@gmail.com>
2026-05-12 22:55:04 +0300

    Зроблено тестування в Postman

--- Migrations/20260512194432_AddPasswordHash.Designer.cs
+++ Migrations/20260512194432_AddPasswordHash.Designer.cs
@@ -3,6 +3,7 @@ using Baza.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Baza.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260512194432_AddPasswordHash")]
+    partial class AddPasswordHash
     {
-        protected override void BuildModel(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260512194432_AddPasswordHash.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Baza.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Players",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Players");
+        }
+    }
+}
