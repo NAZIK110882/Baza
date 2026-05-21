e6a07b5ac341b1eb22213264ddf1b9262418771c
nazar <nazaron11jac@gmail.com>
2026-05-21 23:05:06 +0300

    Дороблено DTOs

---
+++ Models/DTOs/CreateGameSessionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Baza.Models.DTOs
+{
+    public class CreateGameSessionDto
+    {
+        public int PlayerId { get; set; }
+        public string LevelName { get; set; } = string.Empty;
+        public int SurvivalTimeSeconds { get; set; }
+    }
+}
--- Models/DTOs/RegisterPlayerDto.cs
+++ Models/DTOs/RegisterPlayerDto.cs
@@ -6,10 +6,11 @@ namespace Baza.Models.DTOs
     {
         [Required(ErrorMessage = "Нікнейм обов'язковий")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Нікнейм має бути від 3 до 20 символів")]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Нікнейм може містити тільки букви, цифри та підкреслення")]
         public string Nickname { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Пароль обов'язковий")]
-        [MinLength(6, ErrorMessage = "Пароль має бути не менше 6 символів")]
+        [StringLength(32, MinimumLength = 6, ErrorMessage = "Пароль має бути не менше 6 символів")]
         public string Password { get; set; } = string.Empty;
     }
 }
